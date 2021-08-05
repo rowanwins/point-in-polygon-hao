9cee0e8736917d8a8206ece35267fcd4206b9a39
Rowan Winsemius <rowanwins@yahoo.com.au>
2021-08-05 20:23:54 +1000

    Release 1.1.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,8 @@
+
+# 1.1.0
+- Ensure dist folder is ES5 compatible
+- Add typings
+
 # 1.0.0
 - Add support for input validation to ensure rings are closed
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,19 @@
 import {terser} from 'rollup-plugin-terser'
+import buble from '@rollup/plugin-buble'
 
-const output = (file, plugins) => ({
+const output = (file, plugins, format) => ({
     input: './src/index.js',
     output: {
         name: 'pointInPolygon',
         file,
-        format: 'umd',
+        format,
         exports: 'default'
     },
     plugins
 })
 
 export default [
-    output('./dist/pointInPolygon.js', []),
-    output('./dist/pointInPolygon.min.js', [terser()])
+    output('./dist/pointInPolygon.mjs', [buble()], 'es'),
+    output('./dist/pointInPolygon.js', [buble()], 'umd'),
+    output('./dist/pointInPolygon.min.js', [buble(), terser()], 'umd')
 ]
