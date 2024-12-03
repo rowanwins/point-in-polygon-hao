f22ea742a1dea55585c3b7a9497abe22e96179d0
James Beard <james@smallsaucepan.com>
2024-12-03 14:06:13 +1100

    Restoring contents of rollup config after change of filename.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
 import terser from '@rollup/plugin-terser'
 import buble from '@rollup/plugin-buble'
-import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+import copy from 'rollup-plugin-copy'
 
 const output = (file, plugins, format) => ({
     input: './src/index.js',
@@ -10,17 +9,33 @@ const output = (file, plugins, format) => ({
         name: 'pointInPolygon',
         file,
         format,
-        exports: 'default',
-        interop: 'auto',
+        exports: 'default'
     },
     plugins
 })
 
+// Each javascript flavour gets its own copy of the d.ts file, with an 
+// appropriate extension.
 export default [
-    output('./dist/pointInPolygon.mjs', [], 'esm'),
-    output('./dist/pointInPolygon.cjs', [dynamicImportVars({
-        include: 'robust-predicates'
-    }), commonjs()], 'cjs'),
-    output('./dist/pointInPolygon.js', [resolve({resolveOnly: []}), commonjs()], 'umd'),
-    output('./dist/pointInPolygon.min.js', [resolve({resolveOnly: []}), commonjs(), terser()], 'umd')
+    output('./dist/esm/index.js', [
+        buble(),
+        copy({
+            targets: [
+                { src: 'index.d.ts', dest: 'dist/esm' },
+            ]
+        })
+    ], 'esm'),
+    output('./dist/cjs/index.cjs', [
+        // To get around robust-predicates dependency being ESM only, pull it in
+        // and bundle it inline as CJS for this distribution file.
+        resolve(),
+        buble(),
+        copy({
+            targets: [
+                { src: 'index.d.ts', dest: 'dist/cjs', rename: 'index.d.cts' },
+            ]
+        })
+    ], 'cjs'),
+    output('./dist/pointInPolygon.js', [resolve(), buble()], 'umd'),
+    output('./dist/pointInPolygon.min.js', [resolve(), buble(), terser()], 'umd')
 ]
