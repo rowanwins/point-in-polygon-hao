fd5218106bd5795482e3278651d6d3004c0345de
xThrodx <14939634+xThrodx@users.noreply.github.com>
2024-12-20 21:42:35 +0100

    Fix edge case

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ export default function pointInPolygon(p, polygon) {
 
             if (v1 === 0 && v2 === 0) {
                 if ((u2 <= 0 && u1 >= 0) || (u1 <= 0 && u2 >= 0)) return 0
-            } else if ((v2 >= 0 && v1 < 0) || (v2 < 0 && v1 >= 0)) {
+            } else if ((v2 >= 0 && v1 <= 0) || (v2 <= 0 && v1 >= 0)) {
                 f = orient2d(u1, u2, v1, v2, 0, 0)
                 if (f === 0) return 0
                 if ((f > 0 && v2 > 0 && v1 <= 0) || (f < 0 && v2 <= 0 && v1 > 0)) k++
